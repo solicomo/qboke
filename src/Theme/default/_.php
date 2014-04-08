cda55b052b1f3cb1e7ce738ec6551374bf540d05
Soli <soli@cbug.org>
2014-04-08 23:24:50 +0800

    Theme

--- src/Theme/default/_.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * author: Soli <soli@cbug.org>
- * date  : 2013-06-12
- * */
-include_once __DIR__ . '/default.php';
-
-set_theme('default', 'DefaultTheme');
---
+++ src/Theme/iBlog/_.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * author: Soli <soli@cbug.org>
+ * date  : 2013-06-12
+ * */
+namespace QBoke\Theme\iBlog;
+
+set_theme('default', 'QBoke\Theme\iBlog\iBlogTheme');
+set_theme('iblog', 'QBoke\Theme\iBlog\iBlogTheme');
