5fe82cd846fddaa1058951f488485328c950c1bb
Soli <soli@cbug.org>
2014-04-16 02:48:43 +0800

    echo more info

--- src/Installer.php
+++ src/Installer.php
@@ -26,50 +26,54 @@ class Installer
 		// 1. ln -s src/Plugin public/Plugin
 		$target = PLUGIN_DIR;
 		$link   = PUBLIC_DIR . '/Plugin';
-		if (!symlink($target, $link)) {
-			echo "[Fail] ln -s $target $link\n";
-		} else {
+		if (symlink($target, $link)) {
 			echo "[ OK ] ln -s $target $link\n";
+		} else {
+			echo "[Fail] ln -s $target $link\n";
 		}
 
 		// 2. ln -s src/Theme public/Theme
 		$target = THEME_DIR;
 		$link   = PUBLIC_DIR . '/Theme';
-		if (!symlink($target, $link)) {
-			echo "[Fail] ln -s $target $link\n";
-		} else {
+		if (symlink($target, $link)) {
 			echo "[ OK ] ln -s $target $link\n";
+		} else {
+			echo "[Fail] ln -s $target $link\n";
 		}
 
 		// 3. chmod +x src/SCM/Git/git.sh
-		chmod(SCM_DIR . '/Git/git.sh', 0755);
+		if (chmod(SCM_DIR . '/Git/git.sh', 0755)) {
+			echo "[ OK ] chmod +x src/SCM/Git/git.sh\n";
+		} else {
+			echo "[Fail] chmod +x src/SCM/Git/git.sh\n";
+		}
 
 		// 4. chmod
 		$oldumask = umask(0);
 		if (@mkdir(DATA_DIR, 0777, true)) {
-			echo '[Fail] mkdir ' . DATA_DIR . "\n";
-		} else {
 			echo '[ OK ] mkdir ' . DATA_DIR . "\n";
+		} else {
+			echo '[Fail] mkdir ' . DATA_DIR . "\n";
 		}
 
 		if (chmod(CACHE_DIR, 0777)) {
-			echo '[Fail] chmod go+w ' . CACHE_DIR . "\n";
-		} else {
 			echo '[ OK ] chmod go+w ' . CACHE_DIR . "\n";
+		} else {
+			echo '[Fail] chmod go+w ' . CACHE_DIR . "\n";
 		}
 
 		if (chmod(PUBLIC_DIR, 0777)) {
-			echo '[Fail] chmod go+w ' . PUBLIC_DIR . "\n";
-		} else {
 			echo '[ OK ] chmod go+w ' . PUBLIC_DIR . "\n";
+		} else {
+			echo '[Fail] chmod go+w ' . PUBLIC_DIR . "\n";
 		}
 		umask($oldumask);
 
 		// 5. cp config_sample.php config.php
 		if (@copy(ABSPATH . '/config_sample.php', ABSPATH . '/config.php')) {
-			echo "[Fail] cp config_sample.php config.php\n";
-		} else {
 			echo "[ OK ] cp config_sample.php config.php\n";
+		} else {
+			echo "[Fail] cp config_sample.php config.php\n";
 		}
 	}
 }
