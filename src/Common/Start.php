36bb8393f17315e75e06e242278b8b3312d54cfd
Soli <soli@cbug.org>
2015-03-25 23:50:51 +0800

    support dump without pull;

--- src/Common/Start.php
+++ src/Common/Start.php
@@ -36,9 +36,6 @@ class Start
 		trigger_error("got sync request from {$_SERVER['REMOTE_ADDR']}:{$_SERVER['REMOTE_PORT']}", E_USER_NOTICE);
 
 		$g = QBGlobal::getInstance();
-		$repo = $g->config['repo'];
-		$name = $repo['type'];
-		$path = DATA_DIR;
 
 		if ($_GET['key'] !== $g->config['key']) {
 			trigger_error('sync request forbidden.', E_USER_WARNING);
@@ -46,8 +43,28 @@ class Start
 			exit('403 Forbidden');
 		}
 
+		$op = '';
+		if (array_key_exists('op', $_GET) and isset($_GET['op'])) {
+			$op = $_GET['op'];
+		}
+
+		if (empty($op) or $op === 'pull') {
+			self::pull();
+		}
+
+		if (empty($op) or $op === 'dump') {
+			self::dump();
+		}
+	}
+
+	public static function pull()
+	{
+		$g = QBGlobal::getInstance();
+		$repo = $g->config['repo'];
+		$name = $repo['type'];
+		$path = DATA_DIR;
 		$scm_type = $repo['type'];
-		$scm_cls = "\\QBoke\\SCM\\$scm_type\\$scm_type" . 'Scm';
+		$scm_cls  = "\\QBoke\\SCM\\$scm_type\\$scm_type" . 'Scm';
 
 		if (!class_exists($scm_cls)) {
 			trigger_error("no scm [$name]", E_USER_WARNING);
@@ -68,7 +85,10 @@ class Start
 			header('Status: 500 Internal Server Error');
 			exit('500 Internal Server Error');
 		}
+	}
 
+	public static function dump()
+	{
 		$site = new QBSite();
 
 		if ($site->init() === false) {
