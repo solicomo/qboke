437d6953bf3714cf41607c494cc6195c7015f95c
Soli <soli@cbug.org>
2016-03-27 23:11:57 +0800

    update comments plugin;

--- src/Plugin/Comments/CommentsPlugin.php
+++ src/Plugin/Comments/CommentsPlugin.php
@@ -28,28 +28,28 @@ class CommentsPlugin
 		}
 
 		$disqus_shortname = $opts['disqus_shortname'];
+		$disqus_pageurl = $post->url_path();
 		$disqus_identifier = $post->options('disqus_identifier');
-		$disqus_disable_mobile = $opts['disqus_disable_mobile'] == true ? 'true' : 'false';
 
-		if ($disqus_identifier !== false || empty($disqus_identifier)) {
+		if ($disqus_identifier === false || empty($disqus_identifier)) {
 			$disqus_identifier = md5($post->url_path());
 		}
 		?>
 
 		<div id="disqus_thread"></div>
 		<script type="text/javascript">
-			var disqus_shortname = '<?php echo $disqus_shortname; ?>';
-			var disqus_identifier = '<?php echo $disqus_identifier; ?>';
-			var disqus_disable_mobile = '<?php echo $disqus_disable_mobile; ?>';
-
+			var disqus_config = function () {
+				this.page.url = '<?php echo $disqus_pageurl; ?>';
+				this.page.identifier = '<?php echo $disqus_identifier; ?>';
+			};
 			(function() {
-				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				var d = document, s = d.createElement('script');
+				s.src = '//' + $disqus_shortname + '.disqus.com/embed.js';
+				s.setAttribute('data-timestamp', +new Date());
+				(d.head || d.body).appendChild(s);
 			})();
 		</script>
-		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-		<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 		<?php
 	}
 }
